83b470adf23705cc8472479b5bf3408c007f3f9c
reduckted <reduckted@outlook.com>
2026-01-16 20:20:58 +1000

    Initial commit.

---
+++ CS/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+
+namespace CS;
+
+public static class Program {
+
+    public static int Main() {
+        Widget<int> w = new();
+        _ = w.ToList();
+        return 0;
+    }
+
+}
+
+public class Widget<T> : IWidget<T> {
+
+    public int Count => 0;
+
+    public IEnumerator<T> GetEnumerator() => Enumerable.Empty<T>().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+}
+
+public interface IWidget<T> : IReadOnlyCollection<T> { }
---
+++ VB/Program.vb
@@ -0,0 +1,29 @@
+﻿Public NotInheritable Class Program
+
+    Public Shared Function Main(args() As String) As Integer
+        Dim w As New Widget(Of Integer)
+        w.ToList()
+        Return 0
+    End Function
+
+End Class
+
+Public Class Widget(Of T)
+    Implements IWidget(Of T)
+
+    Public ReadOnly Property Count As Integer Implements IReadOnlyCollection(Of T).Count
+
+    Public Function GetEnumerator() As IEnumerator(Of T) Implements IEnumerable(Of T).GetEnumerator
+        Return Enumerable.Empty(Of T)().GetEnumerator()
+    End Function
+
+    Private Function IEnumerable_GetEnumerator() As IEnumerator Implements IEnumerable.GetEnumerator
+        Return GetEnumerator()
+    End Function
+
+End Class
+
+Public Interface IWidget(Of T)
+    Inherits IReadOnlyCollection(Of T)
+
+End Interface
